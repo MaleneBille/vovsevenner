5529563808699caf5fbb8096042ed6f6cf450200
MaleneBille <43337016+MaleneBille@users.noreply.github.com>
2019-05-30 18:41:49 +0200

    Add files via upload

--- template-parts/frontpage-posts.php
+++ template-parts/frontpage-posts.php
@@ -1,16 +1,20 @@
+<!-- Forsidens bootstrap -->
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<?php if (in_category('front-page')) : ?>
-		<div class="col-6">
-			<div class="box-horizontal">
+<?php if (in_category('front-page')) : ?>
+<div class="col-sm-6">
+	<div class="box-horizontal">
 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-				<?php the_post_thumbnail(); // Declare pixel size you need inside the array ?>
+			<?php the_post_thumbnail(); // Declare pixel size you need inside the array ?>
 			<?php endif; ?>
-			<h2><?php the_title(); ?></h2>
-		</a>			
+			<h2>
+				<?php the_title(); ?>
+			</h2>
+		</a>
 	</div>
-	</div>
-	<?php endif; ?>
+</div>
+<?php endif; ?>
 
 <?php endwhile; ?>
 
